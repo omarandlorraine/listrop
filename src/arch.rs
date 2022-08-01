aabdd026efeca2ee30a12672006a83bb3995ddaf
Sam M W <you@example.com>
2022-08-01 11:12:42 +0100

    try to get at strop's business

--- src/arch.rs
+++ src/arch.rs
@@ -1,7 +1,7 @@
 use rust_lisp::model::ForeignValue;
 
 use strop::randomly;
-use strop::machine::mos6502::Mos6502;
+use strop::machine;
 
-impl ForeignValue for Mos6502 {
+impl ForeignValue for machine::Mos6502 {
 }
